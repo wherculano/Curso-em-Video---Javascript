dd425d5d8812913af7347bfbaa80b27096f10ead
Wagner Herculano <wagherculano@hotmail.com>
2019-08-20 10:22:34 -0300

    Update script.js

--- aula_12 - Exercicios/aula_12_4/js/script.js
+++ aula_12 - Exercicios/aula_12_4/js/script.js
@@ -4,8 +4,8 @@ function carregar(){
     var rodape = window.document.getElementById('rodape')
     var cabecalho = document.getElementById('cabecalho')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 9 //variavel para testes
+    var hora = data.getHours()
+    //var hora = 9 //variavel para testes
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12){
         img.src = 'imagens/manha.png'
