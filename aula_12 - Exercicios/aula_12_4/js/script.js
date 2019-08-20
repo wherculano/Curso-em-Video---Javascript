acefe342d949f86c6498608b2bf2a0481b9e48f4
Herculano <wagherculano@hotmail.com>
2019-08-20 09:49:04 -0300

    Aula 12 - Exercicio 01: Pagina de Cumprimento de acordo com a hora do dia.

---
+++ aula_12 - Exercicios/aula_12_4/js/script.js
@@ -0,0 +1,22 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var rodape = window.document.getElementById('rodape')
+    var cabecalho = document.getElementById('cabecalho')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 9 //variavel para testes
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#FDEFB7'
+        rodape.style.color = 'rgba(90, 90, 90, 0.904)'
+        cabecalho.style.color = 'rgba(90, 90, 90, 0.904)'
+    }else if(hora >= 12 && hora <= 18){
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#D75F69'
+    }else{
+        img.src = 'imagens/noite.png'
+        document.body.style.background='#556471'
+    }
+}
