2c01c55ae13b278502f4234d4495e6d046899188
Herculano <wagherculano@hotmail.com>
2019-08-21 08:32:51 -0300

    Aula 13: Estrutura de Repetição - WHILE e DO WHILE

---
+++ aula_13.js
@@ -0,0 +1,13 @@
+//Estrutura de Repetição com Teste Lógico no Início
+cont = 1
+while(cont <= 5){
+    console.log('Olá Mundo!')
+    cont++
+}
+
+//Estrutura de Repetição com Teste Lógico no Final
+i = 1
+do {
+    console.log('Hello World!')
+    i++
+}while(i<=5)
