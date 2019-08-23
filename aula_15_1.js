bde47e9094e6233571118b518cb218df17243cad
Herculano <wagherculano@hotmail.com>
2019-08-23 07:37:23 -0300

    Aula 15: Arrays - Rapida introdução

---
+++ aula_15_1.js
@@ -0,0 +1,20 @@
+//criando um Array
+let num = []
+
+//adicionando valores ao Array
+num.push('banana') //adiciona sempre na ultima posição
+num[2] = 'maçã' //adiciona no indice desejado
+
+//saber tamanho do Array
+tam = num.length
+
+num.push('abacate')
+console.log(num)
+
+num[1] = 'pera'
+
+//ordernar o array
+num.sort()
+
+console.log(num)
+
